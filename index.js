01d2f43af37c50fbada14461b0d56fa72a277a6a
Kyle Corbitt <kyle@corbt.com>
2015-12-22 14:19:21 +0000

    added mapInFrames

--- index.js
+++ index.js
@@ -6,4 +6,29 @@ function nextFrame() {
   });
 }
 
+/* Applies `fn` to each element of `collection`, iterating once per frame */
+nextFrame.mapInFrames = function(collection, fn) {
+  var results = [];
+
+  return new Promise(function(resolve, reject) {
+    var processEntry = function(index) {
+      if (index < collection.length) {
+        requestAnimationFrame(function() {
+          try {
+            results.push(fn(collection[index]));
+            processEntry(index+1);
+          } catch(e) {
+            reject(e);
+          }
+        })
+      }
+      else {
+        resolve(results);
+      }
+    };
+
+    processEntry(0);
+  });
+}
+
 module.exports = nextFrame;
